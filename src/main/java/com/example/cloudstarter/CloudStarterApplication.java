644b76aabb8f3374231308c7a1e78e6b122cdf3b
kokkorko <kostasmaf7@gmail.com>
2025-08-06 13:52:06 +0300

    Initial commit: Spring Boot CRUD app with persistent PostgreSQL, Docker, and Minikube manifests

---
+++ src/main/java/com/example/cloudstarter/CloudStarterApplication.java
@@ -0,0 +1,12 @@
+package com.example.cloudstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStarterApplication.class, args);
+    }
+}
